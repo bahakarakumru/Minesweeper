5d4182a8b4a87d25190fb6ec5e6c39b552777ce6
bahakarakumru <bahakarakumru@gmail.com>
2025-11-08 00:50:44 +0300

    Clean up.

--- Scripts/sketch.js
+++ Scripts/sketch.js
@@ -1,15 +1,4 @@
-class Difficulty{
-    constructor(name, bombCount, width, height){
-        this.name = name;
-        this.bombCount = bombCount;
-        this.width = width;
-        this.height = height;
-    }
-}
-
-var difficultyList = [new Difficulty("Easy", 10, 10, 8), new Difficulty("Medium", 40, 18, 14), new Difficulty("Hard", 99, 24, 20), new Difficulty("Custom", 0, 0, 0)];
-
-var difficultyValue;
+const blockSize = 50;
 
 class Block{
     constructor(x, y){
@@ -19,25 +8,52 @@ class Block{
     }
 }
 
-var blockList = [];
+//i = 0: Custom, i = 1: Easy, i = 2: Medium, i = 3: Hard
+function setDifficulty(i, bombCount, x, y){
+    let difficultySetting = [];
+    
+    switch(i){
+        case 0:
+            difficultySetting.push(bombCount, x, y);
+            break;
+        case 1:
+            difficultySetting.push(10, 10, 8);
+            break;
+        case 2:
+            difficultySetting.push(40, 18, 14);
+            break;
+        case 3:
+            difficultySetting.push(99, 24, 20);
+            break;
+    }
 
-const blockSize = 50;
+    return difficultySetting;
+}
 
-function setup(){
-    difficultyValue = 0;
+//addBlocks() creats a 2D blockList.
+function addBlocks(){
+    let blocks = [];
 
-    //Creats a canvas according to the difficulty.
-    createCanvas(difficultyList[difficultyValue].width * blockSize, difficultyList[difficultyValue].height * blockSize);
-
-    //Adds blocks to the "blockList" as a 2D array.
     for(let y = 0; y < height; y += blockSize){
-        var row = [];
+        let row = [];
 
         for(let x = 0; x < width; x += blockSize)
             row.push(new Block(x, y));
 
-        blockList.push(row);
+        blocks.push(row);
     }
+
+    return blocks;
+}
+
+//setup() runs after every other game.
+function setup(){
+    var difficulty = setDifficulty(1);
+    
+    //Creats a canvas according to the difficulty.
+    createCanvas(difficulty[1]*blockSize, difficulty[2]*blockSize);
+    
+    var blockList = addBlocks();
 }
    
 function draw(){
